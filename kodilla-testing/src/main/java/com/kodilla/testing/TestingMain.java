24360e36adf991cfc23ac753f0f076cae2dd98b6
tomaszRuta <tomasz.ruta@o2.pl>
2020-06-11 16:47:06 +0200

    Exercise 6.2 commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUserName();
 
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator(33,66);
+
+        calculator.add();
+        int addingResult = calculator.add();
+
+        if (calculator.add() == addingResult) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        calculator.substract();
+        int substractionResult = calculator.substract();
+
+        if (calculator.substract() == substractionResult) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+    }
 }
