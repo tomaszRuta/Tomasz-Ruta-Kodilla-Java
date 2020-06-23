e9314b8a53e7716c9a2a6c93df9c4f16a42aec35
tomaszRuta <tomasz.ruta@o2.pl>
2020-06-23 13:34:36 +0200

    Task 6.6 commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,52 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    private List<Book> generateListOfNBooks(int booksQuantity) {
+        List<Book> resultList = new ArrayList<Book>();
+        for(int n = 1; n <= booksQuantity; n++){
+            Book theBook = new Book("Title " + n, "Author " + n, 1970 + n);
+            resultList.add(theBook);
+        }
+        return resultList;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        List<Book> list = new ArrayList<Book>();
+        LibraryUser myUser0Books = new LibraryUser("James", "Bond", "007" );
+        LibraryUser myUser1Books = new LibraryUser("Stefan", "Telefan", "001" );
+        LibraryUser myUser5Books = new LibraryUser("Jan", "Kowalski", "007" );
+
+        if (libraryUser.equals(myUser0Books)) {
+            return list;
+        } else if (libraryUser.equals(myUser1Books)) {
+            list = generateListOfNBooks(1);
+            return list;
+        } else if (libraryUser.equals(myUser5Books)) {
+            list = generateListOfNBooks(5);
+            return list;
+        } else {
+            return list;
+        }
+    }
+}
+
